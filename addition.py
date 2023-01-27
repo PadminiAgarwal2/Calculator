393089f66c8599c0c7d0dd994a69e1758a844d31
Padmini <padmini@hqvfx.com>
2023-01-27 10:53:54 +0530

    calculator first commit

---
+++ addition.py
@@ -0,0 +1,32 @@
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+
+def add(x, y):
+    sum = x + y
+    return sum
+
+
+def subtract(x, y):
+    if (x > y):
+        difference = x - y
+        return difference
+    else:
+        difference = y - x
+        return difference
+
+
+def multiply(x, y):
+    product = x * y
+    return product
+
+
+def divide(x, y):
+    quotient = (x / y)
+    return quotient
+
+
+print(add(x, y))
+print(subtract(x, y))
+print(multiply(x, y))
+print(divide(x, y))
