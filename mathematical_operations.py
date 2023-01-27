682d7141653cbb03b605fd2689f12f230f03b4b9
MonuSisodia <monu.sisodia@hqvfx.in>
2023-01-27 12:59:24 +0530

    commit

--- mathematical_operations.py
+++ mathematical_operations.py
@@ -26,9 +26,6 @@ def divide(x, y):
     return quotient
 
 
-
-
-
 print(add(x, y))
 print(subtract(x, y))
 print(multiply(x, y))
@@ -40,21 +37,19 @@ base = int(input("enter base value "))
 exponent = int(input("enter exponent value "))
 print("Exponential Value is: ", base ** exponent)
 
-
-n=int(input("Enter value:"))
-fact=1
-if n<0:
+n = int(input("Enter value:"))
+fact = 1
+if n < 0:
     print("sorry factorial does not exit")
-elif n==0:
+elif n == 0:
     print("The factorial of 0 and 1")
 else:
-    for i in range(1,n+1):
-      fact=fact*i
-      print("The factorial of",n,"is",fact)
+    for i in range(1, n + 1):
+        fact = fact * i
+        print("The factorial of", n, "is", fact)
 
 # radian to degree code
-pi=22/7
+pi = 22 / 7
 radian = float(input("Input radians: "))
-degree = radian*(180/pi)
+degree = radian * (180 / pi)
 print(degree)
-      
